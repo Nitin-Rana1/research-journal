2a44b9c22ae2c5c4ca2456c07ca581bcb0cb74ba
Nitin Rana <nitinranagraphian@gmail.com>
2022-11-08 16:01:21 +0530

    pl6

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,20 +1,11 @@
 import styles from "./styles/SideBar.module.scss"
 function SideBar() {
     return (
-        <main className={styles.sidebar}>
-            <article>
-                <h3>SearchArticle</h3>
-                <div className={styles.bigLine}>
-                    <div></div>
-                </div>
-            </article>
-            <article>
-                <h3>Member's Area</h3>
-                <span>
-                    Get Register
-                </span>
-                <span>Member Sign In</span>
-            </article>
+        <main>
+            <h1>Helo</h1>
+            <h1>Helo</h1>
+            <h1>Helo</h1>
+
         </main>
     )
 }
