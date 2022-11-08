eb5c1fb9a33a8284304832c880415ab9032611ad
Nitin Rana <nitinranagraphian@gmail.com>
2022-11-08 15:09:07 +0530

    doneui

--- components/SubmitRP.tsx
+++ components/SubmitRP.tsx
@@ -2,6 +2,9 @@ import styles from "./styles/SubmitRP.module.scss"
 function SubmitRP({handleHomePageButton}:any){
     return(
         <main className={styles.main}>
+            <h1>gf gfg fgf gf g</h1>
+            <h1>gf gfg fgf gf g</h1>
+            <h1>gf gfg fgf gf g</h1>
 
             <button onClick={handleHomePageButton}>Home Page</button>
         </main>
