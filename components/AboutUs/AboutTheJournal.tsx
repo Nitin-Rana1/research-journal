e01a1cf19465c2cce5590f91cd4786150a0ef6b8
Nitin Rana <nitinranagraphian@gmail.com>
2022-11-21 12:02:15 +0530

    fix hydration

--- components/AboutUs/AboutTheJournal.tsx
+++ components/AboutUs/AboutTheJournal.tsx
@@ -5,13 +5,13 @@ function AboutTheJournal({ part }: any) {
             <h3>
                 ABOUT THE JOURNAL
             </h3>
-            <p>
+            <div>
                 Graphic Era Journal of Research and Innovation is an open access university level publishing journal that aims to provide and impart knowledge
                 and encourages to explore the world of research and innovation. Writing a journal is a skill and Graphic Era Journal of Research and Innovation provides the
                 opportunity to the students of Graphic Era Hill University to publish their research work and is a frame that allows efficient access to the reviewer. We strive to
                 ensure novelty in each journal. It is a source that provides quality information about recent research and innovations. It provides a platform to the students
                 of the university to grow and enhance the existing knowledge and to make new discoveries and inventions.
-            </p>
+            </div>
 
             <ul className={styles.table}>
                 {["Publication Frequency: 12 per year",
@@ -34,7 +34,7 @@ function AboutTheJournal({ part }: any) {
             <h3>
                 FEATURES OF A JOURNAL
             </h3>
-            <p className={styles.features}>
+            <div className={styles.features}>
                 {["The journal submitted should not be published before for any publication.",
                     "It should be plagiarism free.",
                     "It should focus on specific issue or problem.",
@@ -46,7 +46,7 @@ function AboutTheJournal({ part }: any) {
                             <div key={index}><b>{index + 1}: </b>{value}</div>
                         )
                     })}
-            </p>
+            </div>
         </main>
     )
 }
